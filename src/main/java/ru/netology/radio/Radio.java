ac4fb1578eb35e8a5a4abe500bac689dfe494e85
Denis Petrovskii <denpetrovsky369@gmail.com>
2022-12-03 13:20:36 +0300

    radio1_task

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -1,2 +1,71 @@
+package ru.netology.radio;
+
 public class Radio {
+
+    private int currentRadioStation;
+    private int currentVolume;
+
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public void setCurrentRadioStation(int currentRadioStation) {
+        if (currentRadioStation > 9) {
+            return;
+        }
+        if (currentRadioStation < 0) {
+            return;
+        }
+        this.currentRadioStation = currentRadioStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > 10) {
+            return;
+        }
+        if (currentVolume < 0) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+
+    public void next() {
+        if (currentRadioStation < 9) {
+            currentRadioStation++;
+        } else {
+            currentRadioStation = 0;
+        }
+    }
+
+    public void prev() {
+        if (currentRadioStation > 0) {
+            currentRadioStation--;
+        } else {
+            currentRadioStation = 9;
+        }
+    }
+
+    public void volumePlus() {
+        if (currentVolume < 10) {
+            currentVolume++;
+        } else {
+            currentVolume = 10;
+        }
+    }
+
+    public void volumeMinus() {
+
+        if (currentVolume > 0) {
+            currentVolume--;
+
+        } else {
+            currentVolume = 0;
+        }
+    }
 }
--- src/test/java/ru.netology.radio/RadioTest.java
+++ src/test/java/ru.netology.radio/RadioTest.java
@@ -1,7 +1,135 @@
 package ru.netology.radio;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
 
+    Radio radio = new Radio();
+
+    // тестируем объекты радио
+    @Test
+    public void currentRadioStationеUnderMaxOverMin() {
+        radio.setCurrentRadioStation(5);
+        assertEquals(5, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void currentRadioStationеOverMax() {
+        radio.setCurrentRadioStation(10);
+        assertEquals(0, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void currentRadioStationеUnderMin() {
+        radio.setCurrentRadioStation(-1);
+        assertEquals(0, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void currentVolumeUnerMaxOverMin() {
+        radio.setCurrentVolume(7);
+        assertEquals(7, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void currentVolumeOverMax() {
+        radio.setCurrentVolume(11);
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void currentVolumeUnderMin() {
+        radio.setCurrentVolume(-1);
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+    // тестируем переключение станций
+    @Test
+    public void changeOverLastRadioStation() {
+        radio.setCurrentRadioStation(9);
+        radio.next();
+        assertEquals(0, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void changeUnderFirstRadioStation() {
+        radio.setCurrentRadioStation(0);
+        radio.prev();
+        assertEquals(9, radio.getCurrentRadioStation());
+
+    }
+
+    @Test
+    public void nextRadioStation() {
+        radio.setCurrentRadioStation(6);
+        radio.next();
+        assertEquals(7, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void prevRadioStation() {
+        radio.setCurrentRadioStation(4);
+        radio.prev();
+        assertEquals(3, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void changeOverPenultRadioStation() {
+        radio.setCurrentRadioStation(8);
+        radio.next();
+        assertEquals(9, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void changeUnderAfterTheFirstRadioStation() {
+        radio.setCurrentRadioStation(1);
+        radio.prev();
+        assertEquals(0, radio.getCurrentRadioStation());
+    }
+
+    // тестируем переключение громкости
+    @Test
+    public void volumeOverMax() {
+        radio.setCurrentVolume(10);
+        radio.volumePlus();
+        assertEquals(10, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void volumeDownUnderMin() {
+        radio.setCurrentVolume(0);
+        radio.volumeMinus();
+        assertEquals(0, radio.getCurrentVolume());
+
+    }
+
+    @Test
+    public void plusVolume() {
+        radio.setCurrentVolume(4);
+        radio.volumePlus();
+        assertEquals(5, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void minusVolume() {
+        radio.setCurrentVolume(7);
+        radio.volumeMinus();
+        assertEquals(6, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void volumeDownUnderAfterMin() {
+        radio.setCurrentVolume(1);
+        radio.volumeMinus();
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void volumeOverPenultMax() {
+        radio.setCurrentVolume(9);
+        radio.volumePlus();
+        assertEquals(10, radio.getCurrentVolume());
+    }
 }
